18a6b3c307e10c2cf3e927ba380e8fd217196097
Daniel Reza <daniel.reza1007@gmail.com>
2021-02-14 19:00:59 -0600

    first commit and route set up

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+// for routes
+
+app.use(require("./Develop/public/api.js"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/Develop/public/index.html"));
+  });
+
+app.listen(PORT, () => {
+    console.log(`We got port ${PORT} in da house!`)
+});
